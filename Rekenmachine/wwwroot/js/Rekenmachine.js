b6cbbc4b4781a99a3ac5330f575db60bb585d03b
William Van Daele <r0848828@ucll.be>
2020-12-15 15:34:30 +0100

    Add project files.

---
+++ Rekenmachine/wwwroot/js/Rekenmachine.js
@@ -0,0 +1,21 @@
+﻿var input = document.getElementById('input');
+
+function button(obj) {
+
+    var pushed = obj.innerHTML;
+
+    if (pushed == '=') {
+        input.innerHTML = eval(input.innerHTML);
+    }
+    else if (pushed == 'AC') {
+        input.innerHTML = '0';
+    }
+    else {
+        if (input.innerHTML == '0') {
+            input.innerHTML = pushed;
+        }
+        else {
+            input.innerHTML += pushed;
+        }
+    }
+}
